99fe207f6621a7e9b86600762c53b027fcde0eee
TheZeronet <Virajbiji01@gmail.com>
2024-03-07 23:21:14 +0530

    mewing

--- client/src/pages/monu/pages/LandingPage.jsx
+++ client/src/pages/monu/pages/LandingPage.jsx
@@ -1,21 +1,13 @@
 import React from "react";
-import BigGallery from "../components/BigGallery";
-import Choose from "../components/Choose";
 import FrontCarousel from "../components/FrontCarousel";
-
-import Girl from "../components/Girl";
-import Pricing from "../components/Pricing";
 import Store from "../components/Store";
 import "../styles/LandingPage.css";
-import Body from "./body";
 
 const LandingPage = () => {
   return (
     <div>
-      {/* <Body /> */}
       <FrontCarousel />
       <Store />
-      {/* <Choose /> */}
     </div>
   );
 };
